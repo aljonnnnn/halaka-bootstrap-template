0c4da5a7479a7c89f1e2ad7be07513e22afb7c02
aljonnnnn <delumenaljon@gmail.com>
2021-02-03 12:55:06 +0800

    add footer

--- js/script.js
+++ js/script.js
@@ -4,3 +4,8 @@ const navMenu = document.querySelector('.nav-menu');
 hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('show');
 })
+
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle('on-scroll', window.scrollY > 0);
+});
