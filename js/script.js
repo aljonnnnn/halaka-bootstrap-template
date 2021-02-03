2177bf9cf2ff8197dccb85765d9fd8a572e241a8
aljonnnnn <delumenaljon@gmail.com>
2021-02-03 18:54:23 +0800

    add scrollit js, and remove style cdn and js cdn

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-
+// menu button
 hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('show');
+    hamburger.classList.add('is-active');
 })
 
+// add class in header
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle('on-scroll', window.scrollY > 0);
 });
+
+// scroll it call
+$.scrollIt({
+    topOffset: -50
+})
+
+// close menu when click a navlink, magic to hahahahahaha
+function hideMenu() {
+    const menuBars = document.querySelector('.is-active');
+    if (menuBars) {
+        hamburger.classList.remove('is-active')
+        navMenu.classList.remove('show')
+    }
+}
+
+navMenu.addEventListener('click', hideMenu);
